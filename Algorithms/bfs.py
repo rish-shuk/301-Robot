d2fc69ac1cdc34f3b929e954af1f4f21991aa440
Job Josef Santos <job.santos924@gmail.com>
2023-08-02 13:46:34 +1200

    feat: added map from student pack

---
+++ Algorithms/bfs.py
@@ -0,0 +1,24 @@
+# Method 1: Using nested lists
+rows = 15
+columns = 19
+
+# Initializing the array with zeros
+array = [[0 for _ in range(columns)] for _ in range(rows)]
+
+# Print the array
+for row in array:
+    print(f'{row}, ')
+
+
+
+def GetMapFromFile(filepath = '\student-pack-stuff\matlab\map_1.txt'):
+    # ASSUMPTIONS: Map is always 15 (rows) x 19 (columns) and ends with a new line
+
+    
+    # 1 == Path, 0 == Cannot got
+
+    # get map txt from file path
+    # read map file and convert into 2d array
+    # return 2d array
+
+    return 
---
+++ Algorithms/read-before-running.md
@@ -0,0 +1,16 @@
+## Prerequisites:
+
+### Install C and C++ compilers
+
+https://www.freecodecamp.org/news/how-to-install-c-and-cpp-compiler-on-windows/
+
+### C for VSCode
+
+https://www.freecodecamp.org/news/how-to-write-and-run-c-cpp-code-on-visual-studio-code/
+
+### VSCode extensions
+
+- C/C++
+- Code Runner
+
+> Change extension settings 'Code-runner: Run In Terminal' to ticked.
