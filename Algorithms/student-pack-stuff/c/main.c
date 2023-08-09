ab70be3cf3a48b54aecfc714adb6027ade93d0b0
Ian Kuik <100252099+ikui753@users.noreply.github.com>
2023-08-09 15:25:01 +1200

    begin implementation of test functions

---
+++ Algorithms/student-pack-stuff/c/main.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define ROWS 15
+#define COLS 19
+
+void readMap(const char *fileName, int map[ROWS][COLS]) {
+    FILE *file = fopen(fileName, "r");
+
+    if (file == NULL) {
+        perror("Error opening file");
+        exit(1); // Exit the program if file opening fails
+    }
+
+    for (int i = 0; i < ROWS; i++) {
+        for (int j = 0; j < COLS; j++) {
+            if (fscanf(file, "%d", &map[i][j]) != 1) {
+                fprintf(stderr, "Error reading data from file");
+                fclose(file);
+                exit(1); // Exit the program if reading data fails
+            }
+        }
+    }
+
+    fclose(file); // Close text file
+}
+
+int main() {
+    int map[ROWS][COLS];
+    readMap("map1.txt", map);
+
+    // Print the read array for verification
+    for (int i = 0; i < ROWS; i++) {
+        for (int j = 0; j < COLS; j++) {
+            printf("%d ", map[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
