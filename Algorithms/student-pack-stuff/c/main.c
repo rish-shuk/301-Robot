5019356b3d6fd65a99e69d7edd046ebb03d35fbd
Ian Kuik <100252099+ikui753@users.noreply.github.com>
2023-08-09 16:43:41 +1200

    properly outputting map

--- Algorithms/student-pack-stuff/c/main.c
+++ Algorithms/student-pack-stuff/c/main.c
@@ -12,10 +12,18 @@ void readMap(char *filePath, int map[ROWS][COLS]) {
         exit(1); // Exit the program if file opening fails
     }
 
+    // Read the file and populate the array
     for (int i = 0; i < ROWS; i++) {
         for (int j = 0; j < COLS; j++) {
-            int ch = fgetc(file);
-            map[i][j] = ch;
+            char c;
+            do {
+                c = fgetc(file);
+            } while (c != '0' && c != '1' && c != EOF); // Skip characters that are not '0' or '1'
+            if (c == EOF) {
+                fclose(file);
+                return; // Exit the function if end of file is reached
+            }
+            map[i][j] = c - '0';  // Convert character to integer
         }
     }
 
@@ -26,16 +34,13 @@ int main() {
     int map[ROWS][COLS];
     readMap("map_1.txt", map);
 
-    // Print the characters in the map without newlines
+    // Print the map in a grid format
     for (int i = 0; i < ROWS; i++) {
         for (int j = 0; j < COLS; j++) {
-            if (map[i][j] != '\n') {
-                printf("%c", map[i][j]);
-            }
+            printf("%d", map[i][j]);
         }
         printf("\n");
     }
-
 
     return 0;
 }
