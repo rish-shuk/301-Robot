a789be3358ffa035f389bf46716715cb259ce30b
iankuik1 <112829317+iankuik1@users.noreply.github.com>
2023-08-15 11:43:49 +1200

    added usbUART and movement header files

--- psoc student pack/psoc_code_base/CS301_Class.cydsn/main.c
+++ psoc student pack/psoc_code_base/CS301_Class.cydsn/main.c
@@ -23,6 +23,7 @@
 #include "vars.h"
 #include "pathfinding.h"
 #include "initialise.h"
+#include "movement.h"
 
 //* ========================================
 void usbPutString(char *s);
@@ -92,56 +93,6 @@ int main()
     }   
 }
 
-// Direction/ Movement macros
-//* ========================================
-// stop moving
-void stopMoving() {
-    PWM_1_WriteCompare(50);
-    PWM_2_WriteCompare(50);
-}
-
-// rotate anticlockwise 90deg
-void rotationAntiClockwise() {
-    PWM_1_WriteCompare(35);
-    PWM_2_WriteCompare(35);
-    
-    int quadPulseCount = 0;
-    QuadDec_M1_SetCounter(0);
-    while(quadPulseCount > -105) {
-        quadPulseCount = QuadDec_M1_GetCounter();  
-    }
-    QuadDec_M1_SetCounter(0);
-    //CyDelay(500);
-    //stopMoving(); // stop movement, ready for next instruction
-}
-
-// rotate clockwise 90deg
-void rotationClockwise() {
-    PWM_1_WriteCompare(65);
-    PWM_2_WriteCompare(65);
-    
-    int quadPulseCount = 0;
-    QuadDec_M1_SetCounter(0);
-    while(quadPulseCount < 110) {
-        quadPulseCount = QuadDec_M1_GetCounter();
-    }
-    QuadDec_M1_SetCounter(0);
-    //CyDelay(500); // change to quadrature encoder pulses, rather than time delay
-    //stopMoving(); // stop movement, ready for next instruction
-}
-
-// move forward
-void directionForward() {
-    PWM_1_WriteCompare(75);
-    PWM_2_WriteCompare(25);
-}
-
-// move backward
-void directionBackward() {
-    PWM_1_WriteCompare(25);
-    PWM_2_WriteCompare(75);
-}
-
 // Calculations
 //* ========================================
 void quadCountToRPM(uint16 count)
@@ -152,106 +103,5 @@ void quadCountToRPM(uint16 count)
     //usbPutString(buffer);
     //usbPutString("rpm ");
 }
-
-// USBUART Operations
-//* ========================================
-void usbPutString(char *s)
-{
-// !! Assumes that *s is a string with allocated space >=64 chars     
-//  Since USB implementation retricts data packets to 64 chars, this function truncates the
-//  length to 62 char (63rd char is a '!')
-
-#ifdef USE_USB     
-    while (USBUART_CDCIsReady() == 0);
-    s[63]='\0';
-    s[62]='!';
-    
-    // processing for changing duty cycle- look for 'p'' ''xxx'
-    if(s[0] == 112 && s[1] == 32) {
-        // get percentage and change duty cycle based on user input
-        char percentageArray[3] = {s[2], s[3], s[4]};  
-        float percentage = (uint8) percentageArray[0] * 100 + (uint8) percentageArray[1] * 10 + (uint8) percentageArray[2];
-        // concatenate elements as uint8
-        changeDutyCycle(percentage);
-    }
-    
-    USBUART_PutData((uint8*)s,strlen(s));
-    
-#endif
-}
-//* ========================================
-void usbPutChar(char c)
-{
-#ifdef USE_USB     
-    while (USBUART_CDCIsReady() == 0);
-    USBUART_PutChar(c);
-#endif    
-}
-//* ========================================
-// command interpreter to alter duty cycle via terminal
-void changeDutyCycle(float percentage) 
-{
-    // perform calculaton
-    // get period
-    float period = 255;
-    // changes the compare value to change duty cycle
-    float newPeriod = ((percentage/100.0) * period);
-    PWM_1_WriteCompare((uint8)newPeriod); 
-   
-}
-//* ========================================
-void handle_usb()
-{
-    // handles input at terminal, echos it back to the terminal
-    // turn echo OFF, key emulation: only CR
-    // entered string is made available in 'line' and 'flag_KB_string' is set
-    
-    static uint8 usbStarted = FALSE;
-    static uint16 usbBufCount = 0;
-    uint8 c; 
-    
-
-    if (!usbStarted)
-    {
-        if (USBUART_GetConfiguration())
-        {
-            USBUART_CDC_Init();
-            usbStarted = TRUE;
-        }
-    }
-    else
-    {
-        if (USBUART_DataIsReady() != 0)
-        {  
-            c = USBUART_GetChar();
-
-            if ((c == 13) || (c == 10))
-            {
-//                if (usbBufCount > 0)
-                {
-                    entry[usbBufCount]= '\0';
-                    strcpy(line,entry);
-                    usbBufCount = 0;
-                    flag_KB_string = 1;
-                }
-            }
-            else 
-            {
-                if (((c == CHAR_BACKSP) || (c == CHAR_DEL) ) && (usbBufCount > 0) )
-                    usbBufCount--;
-                else
-                {
-                    if (usbBufCount > (BUF_SIZE-2) ) // one less else strtok triggers a crash
-                    {
-                       USBUART_PutChar('!');        
-                    }
-                    else
-                        entry[usbBufCount++] = c;  
-                }  
-            }
-        }
-    }    
-}
-
 
 /* [] END OF FILE */
---
+++ psoc student pack/psoc_code_base/CS301_Class.cydsn/movement.h
@@ -0,0 +1,56 @@
+/* ========================================
+ *
+ * Copyright YOUR COMPANY, THE YEAR
+ * All Rights Reserved
+ * UNPUBLISHED, LICENSED SOFTWARE.
+ *
+ * CONFIDENTIAL AND PROPRIETARY INFORMATION
+ * WHICH IS THE PROPERTY OF your company.
+ *
+ * ========================================
+*/
+#include "project.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+// Direction/ Movement macros
+//* ========================================
+// stop moving
+void stopMoving() {
+    PWM_1_WriteCompare(50);
+    PWM_2_WriteCompare(50);
+}
+
+// rotate clockwise 90deg
+void rotationClockwise() {
+    PWM_1_WriteCompare(65);
+    PWM_2_WriteCompare(65);
+    
+    int quadPulseCount = 0;
+    QuadDec_M1_SetCounter(0);
+    while(quadPulseCount < 110) {
+        quadPulseCount = QuadDec_M1_GetCounter();
+    }
+    QuadDec_M1_SetCounter(0);
+    //CyDelay(500); // change to quadrature encoder pulses, rather than time delay
+    //stopMoving(); // stop movement, ready for next instruction
+}
+
+// rotate anticlockwise 90deg
+void rotationAntiClockwise() {
+    PWM_1_WriteCompare(35);
+    PWM_2_WriteCompare(35);
+    
+    int quadPulseCount = 0;
+    QuadDec_M1_SetCounter(0);
+    while(quadPulseCount > -105) {
+        quadPulseCount = QuadDec_M1_GetCounter();  
+    }
+    QuadDec_M1_SetCounter(0);
+    //CyDelay(500);
+    //stopMoving(); // stop movement, ready for next instruction
+}
+
+/* [] END OF FILE */
