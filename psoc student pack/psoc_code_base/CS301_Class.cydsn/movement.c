8d4d9958712ab5decbab08161eee0100e47805c1
Rishi Shukla <71300397+rish-shuk@users.noreply.github.com>
2023-09-19 16:00:16 +1200

    refactor: changed movement func names

--- psoc student pack/psoc_code_base/CS301_Class.cydsn/movement.c
+++ psoc student pack/psoc_code_base/CS301_Class.cydsn/movement.c
@@ -15,6 +15,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <math.h>
 
 #define MAX_MOVEMENT_PERCENTAGE 1.00 // 100% of PWM COUNT
 
@@ -26,13 +27,13 @@ uint16 turnCount = 0;
 uint16 maxTurnCount = 25; // clock counts for one complete 90 deg turn
 
 // Calculate Speed
-uint16 setPWM1speed(float percentage)
+uint16 setPWMAbove50(float percentage)
 {
     uint16 compareCount = round((0.5 + percentage) * MAX_PWM_COUNT * MAX_MOVEMENT_PERCENTAGE);
     return compareCount;
 }
 
-uint16 setPWM2speed(float percentage) 
+uint16 setPWMBelow50(float percentage) 
 {
     uint16 compareCount = round((0.5 - percentage) * MAX_PWM_COUNT * MAX_MOVEMENT_PERCENTAGE);
     return compareCount;
@@ -53,60 +54,19 @@ uint16 setPWM2speed(float percentage)
 // stop moving
 void stopMoving()
 {
-    PWM_1_WriteCompare(setPWM1speed(0)); // 50%
-    PWM_2_WriteCompare(setPWM2speed(0)); // 50%
+    PWM_1_WriteCompare(setPWMAbove50(0)); // 50%
+    PWM_2_WriteCompare(setPWMBelow50(0)); // 50%
 
     //PWM_1_WriteCompare(calculateSpeedCompareCount(50)); // 50%
     // PWM_2_WriteCompare(calculateSpeedCompareCount(50)); // 50%
-}
-
-// rotate clockwise 90deg
-void rotationClockwise()
-{
-    PWM_1_WriteCompare(setPWM1speed(15)); // 65% (50% + 15%) // Left Wheel Clockwise
-    // THE REASON WHY WE USE PWM1 IS BECAUSE FOR PWM_2, WHEN >50%, IT SPINS ANTICLOCKWISE 
-    PWM_2_WriteCompare(setPWM1speed(15)); // 65% // Right Wheel AntiClockwise
-
-    // PWM_1_WriteCompare(calculateSpeedCompareCount(65)); // 65%
-    // PWM_2_WriteCompare(calculateSpeedCompareCount(65)); // 65%
-
-    int quadPulseCount = 0;
-    QuadDec_M1_SetCounter(0);
-    while (quadPulseCount < 110)
-    {
-        quadPulseCount = QuadDec_M1_GetCounter();
-    }
-    QuadDec_M1_SetCounter(0);
-    // CyDelay(500); // change to quadrature encoder pulses, rather than time delay
-    // stopMoving(); // stop movement, ready for next instruction
-}
-
-// rotate anticlockwise 90deg- CHANGE TO NOT USE TIMER BUT SIMPLY CHECK FOR NEW CONDITIONS????
-void rotationAntiClockwise()
-{
-    PWM_1_WriteCompare(22937); // 35%
-    PWM_2_WriteCompare(22937); // 35%
-
-    // PWM_1_WriteCompare(calculateSpeedCompareCount(35)); // 35%
-    // PWM_2_WriteCompare(calculateSpeedCompareCount(35)); // 35%
-
-    int quadPulseCount = 0;
-    QuadDec_M1_SetCounter(0);
-    while (quadPulseCount > -105)
-    {
-        quadPulseCount = QuadDec_M1_GetCounter();
-    }
-    QuadDec_M1_SetCounter(0);
-    // CyDelay(500);
-    // stopMoving(); // stop movement, ready for next instruction
 }
 
 // keep rotating clockwise
 // used for adjusting deviations
 void adjustRight()
 {
-    PWM_1_WriteCompare(setPWM1speed(PWM_Forward_Percentage)); // 65% - Left Wheel Clockwise
-    PWM_2_WriteCompare(setPWM2speed(PWM_Forward_Percentage + 0.05)); // 40% - Right Wheel Clockwise
+    PWM_1_WriteCompare(setPWMAbove50(PWM_Forward_Percentage)); // 65% - Left Wheel Clockwise
+    PWM_2_WriteCompare(setPWMBelow50(PWM_Forward_Percentage + 0.05)); // 40% - Right Wheel Clockwise
     // PWM_1_WriteCompare(calculateSpeedCompareCount(65)); // 65%
     // PWM_2_WriteCompare(calculateSpeedCompareCount(40)); // 40%
 }
@@ -115,8 +75,8 @@ void adjustRight()
 // used for adjusting deviations
 void adjustLeft()
 {
-    PWM_1_WriteCompare(setPWM1speed(PWM_Forward_Percentage - 0.05)); // 60% - Left Wheel Clockwise
-    PWM_2_WriteCompare(setPWM2speed(PWM_Forward_Percentage)); // 35% - Right Wheel Clockwise
+    PWM_1_WriteCompare(setPWMAbove50(PWM_Forward_Percentage - 0.05)); // 60% - Left Wheel Clockwise
+    PWM_2_WriteCompare(setPWMBelow50(PWM_Forward_Percentage)); // 35% - Right Wheel Clockwise
     // PWM_1_WriteCompare(calculateSpeedCompareCount(60)); // 60%
     // PWM_2_WriteCompare(calculateSpeedCompareCount(35)); // 35%
     //  love cock and balls
@@ -142,8 +102,8 @@ void turnRight()
 
 void moveForward()
 {
-    PWM_1_WriteCompare(setPWM1speed(PWM_Forward_Percentage)); // 65% - Left Wheel Clockwise
-    PWM_2_WriteCompare(setPWM2speed(PWM_Forward_Percentage)); // 35% - Right Wheel Clockwise
+    PWM_1_WriteCompare(setPWMAbove50(PWM_Forward_Percentage)); // 65% - Left Wheel Clockwise
+    PWM_2_WriteCompare(setPWMBelow50(PWM_Forward_Percentage)); // 35% - Right Wheel Clockwise
     // PWM_1_WriteCompare(calculateSpeedCompareCount(65)); // 65%
     // PWM_2_WriteCompare(calculateSpeedCompareCount(35)); // 35%
 }
--- psoc student pack/psoc_code_base/CS301_Class.cydsn/movement.h
+++ psoc student pack/psoc_code_base/CS301_Class.cydsn/movement.h
@@ -1,4 +1,4 @@
-/* ========================================
+    /* ========================================
  *
  * Copyright YOUR COMPANY, THE YEAR
  * All Rights Reserved
@@ -19,18 +19,12 @@
 #include <stdlib.h>
 
 // Speed Macros
-uint16 setPWM1speed(float percentage);
-uint16 setPWM2speed(float percentage);
+uint16 setPWMAbove50(float percentage);
+uint16 setPWMBelow50(float percentage);
 // Direction/ Movement macros
 //* ========================================
 // Stop moving
 void stopMoving();
-
-// Rotate clockwise 90 degrees
-void rotationClockwise();
-
-// Rotate anticlockwise 90 degrees
-void rotationAntiClockwise();
 
 // keep rotating clockwise
 void adjustRight();
